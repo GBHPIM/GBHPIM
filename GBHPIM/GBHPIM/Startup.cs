5d5792c25b755866a4942bc14da5b4c39e995f70
WhiteTeamUSA <37628036+WhiteTeamUSA@users.noreply.github.com>
2018-05-12 11:43:01 -0400

    First

---
+++ GBHPIM/GBHPIM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GBHPIM.Startup))]
+namespace GBHPIM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
